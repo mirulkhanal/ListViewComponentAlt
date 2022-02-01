d145c29fd4257a850365bd2d768d69024619480b
Mirul Khanal <mirulkhanal@gmail.com>
2022-02-01 12:16:13 +0545

    commented on code

--- src/components/DataTreeComponent.js
+++ src/components/DataTreeComponent.js
@@ -9,21 +9,27 @@ const DataTreeComponent = ({ categories, productsData }) => {
   const [productsByCategory, setProductsByCategory] = useState();
 
   const handleClick = (item) => {
+    // checks if the category item has child
     item.child ? setNextChild(item.child) : setMappingCategories([item]);
+
+    // on every click, checks if the item has related products
     const productsArray = productsData.filter(
       (product) => product.goodsCategory === item._id
     );
+    // sets the products in the state to update onb every click
     setProductsByCategory(productsArray);
   };
 
   return (
     <div>
+      {/* if there is child it renders the child otherwise it renders the default nextChild which consists of categories array by default */}
       {nextChild &&
         nextChild.map((item) => (
           <button onClick={() => handleClick(item)} key={item._id}>
             {item.name}
           </button>
         ))}
+      {/* conditionally renders products based on their category */}
       {productsByCategory &&
         productsByCategory.map((product) => <h1>{product.name}</h1>)}
     </div>
